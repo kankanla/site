c45dab2e5040d899375cecda6ffaad3cae809191
kankanla <koyi_s@hotmail.com>
2016-04-17 02:59:23 +0900

    Added files via upload

---
+++ IDS.php
@@ -0,0 +1,19 @@
+<?php
+
+// readme
+
+// DIV ID
+// S0100	S0100_main_document_artist_card
+// S0110	S0110_from_lid
+// S0101	S0101_main_document_artist_info
+// S0102	S0102_main_artist_top_left
+// S0103	S0103_mini_card_area
+// S0104	S0104_mini_card
+
+
+
+
+
+// ERRORCODE
+// #20001	include_searchform.php
+?>
